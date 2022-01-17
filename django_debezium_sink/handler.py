03085f26a208f47093e5e38f024c4d16d5247acf
taher-systango <tmalubhaiwala@isystango.com>
2022-01-17 20:11:56 +0530

    Add comments

--- django_debezium_sink/handler.py
+++ django_debezium_sink/handler.py
@@ -21,6 +21,9 @@ logger.setLevel(logging.DEBUG)
 
 
 class DebeziumHandler:
+    """
+    Main class for handling all django-debezium-sink functionality.
+    """
     def __init__(self):
         self._models = []
         self.topics = self.validate_topics(self.generate_topics())
@@ -30,6 +33,10 @@ class DebeziumHandler:
         self.pool = Pool(processes=cpu_count - 1)
 
     def listen(self):
+        """
+        Start listening to Kafka using the settings defined at settings.py
+        :return:
+        """
         try:
             self.log(f'Creating Kafka consumer at {settings.DDS_BROKER_HOST}:{settings.DDS_BROKER_PORT}', 'success')
             self.log(f'Consuming topics: {self.topics}', 'success')
@@ -49,9 +56,20 @@ class DebeziumHandler:
 
     @staticmethod
     def send_message(model, payload):
+        """
+        Call debezium_updates django signal to send payload to all the subscribers of that model
+        :param model:
+        :param payload:
+        :return:
+        """
         debezium_updates.send(sender=model, payload=payload)
 
     def generate_result(self, payload):
+        """
+        Convert dict and nested dicts to dataclass
+        :param payload:
+        :return:
+        """
         result = Result()
         for key in payload:
             if type(payload[key]) == dict:
@@ -62,6 +80,11 @@ class DebeziumHandler:
         return result
 
     def process_message(self, msg):
+        """
+        Data parsing and message sending
+        :param msg:
+        :return:
+        """
         payload = self.extract_payload(msg.value())
         # pprint.pprint(payload)
         model = self.get_model_from_payload(payload)
@@ -70,6 +93,11 @@ class DebeziumHandler:
         # DebeziumHandler.send_message(model, result)
 
     def get_model_from_payload(self, payload):
+        """
+        Extract database table name from the payload and get associated model from instance
+        :param payload:
+        :return:
+        """
         table_name = payload.get('source').get('table')
         return self._models[table_name]
 
@@ -77,6 +105,10 @@ class DebeziumHandler:
         return json.loads(value).get('payload')
 
     def generate_topics(self):
+        """
+        Generate topic names using the information provided in settings
+        :return:
+        """
         server_name = settings.DDS_DEBEZIUM_CONNECTOR_SERVER_NAME
         schema = settings.DDS_DATABASE_SCHEMA
         prefix = server_name
@@ -86,6 +118,10 @@ class DebeziumHandler:
         return [prefix + model._meta.app_label + '_' + model._meta.model_name for model in models]
 
     def parse_models(self):
+        """
+        Resolve and parse settings.DDS_MODELS into self._models
+        :return:
+        """
         self._models = {}
         if len(settings.DDS_MODELS) == 0:
             raise ImproperlyConfigured('DDS_MODELS setting not found or found an empty iterable')
@@ -100,6 +136,11 @@ class DebeziumHandler:
         return self._models.values()
 
     def validate_topics(self, topics: list):
+        """
+        Verify local topics with topics present in Kafka
+        :param topics:
+        :return:
+        """
         consumer = self.create_consumer('DDS-validate-topics')
         server_topics = consumer.list_topics().topics.keys()
         if len(set(topics) - set(server_topics)) > 0:
@@ -107,6 +148,11 @@ class DebeziumHandler:
         return list(set(topics) & set(server_topics))
 
     def create_consumer(self, group=None):
+        """
+        Create Kafka consumer
+        :param group:
+        :return:
+        """
         if not group:
             group = settings.DDS_KAFKA_CONSUMER_GROUP
         broker = f'{settings.DDS_BROKER_HOST}:{settings.DDS_BROKER_PORT}'
@@ -124,4 +170,7 @@ class DebeziumHandler:
 
 @dataclass
 class Result:
+    """
+    This gets populated dynamically with payload
+    """
     pass
